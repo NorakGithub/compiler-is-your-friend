6532aa2c779f88143717d4d31068e3d650fa1a65
Khemanorak Khath <khath.khemanorak@gmail.com>
2020-08-05 14:00:09 +0700

    Added answer

--- 1.correctness.ts
+++ 1.correctness.ts
@@ -12,3 +12,13 @@ Questions:
 1) How to ensure that the input is a string at compile time?
 2) What did you learn from this exercise?
 */
+
+function scriptAt(value: string): number {
+    return value.indexOf('Script');
+}
+console.log(scriptAt('this is Script'));
+
+/* Answer
+1) We must define type for parameter and return, so typescript know what type to accept.
+2) Always use type, so that it could prevent unexpected bugs.
+*/
--- 2.immutability.ts
+++ 2.immutability.ts
@@ -11,11 +11,21 @@ Questions:
 3) What did you learn from this exercise?
 */
 function safeDivide(): number {
-    let x: number = 42;
+    const x: number = 42;
 
     if (x == 0) throw new Error("x should not be 0");
 
-    x = x - 42;
+    // x = x - 42;
 
     return 42 / x;
 }
+
+console.log(safeDivide());
+
+/*
+1) Line 18 cause divide by zero because x variable is mutable
+2) Now we change line 14 from let to const instead so x is immutable
+2.1) Change let to const
+2.2) Compile error when trying to mutate
+3) The important of immutability when in practice, because we will need to manage state. And debugging could be hard since we have to findout where the state changed.
+*/
